84d4423fdda8aa1b4ce9f6b8b4fac238ffd564b0
antikytheraton <binauralvoice@gmail.com>
2017-06-19 17:03:33 -0500

    lol

--- example/config.py
+++ example/config.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    'FACEBOOK_TOKEN': 'EAAbYZAtdQWjMBANtXE21lgxdqNFePXQ0ZC7a1DHCTt1apPvnLNzDU37DSQJjZCtqGRbI7P4MWXRYTmZCKm8HsfpmqzNq81PVLXoZADTPcZA1roZCwnvyilEQkB3HuCRlmPqmZCMnGQwB8sLYuEQdU0drLTNg4WSc9Flu56XQLRRBjgZDZD',
+    'FACEBOOK_TOKEN': 'EAAbYZAtdQxxxxxxxxxxxxxxxxxxxxxNg4WSc9Flu56XQLRRBjgZDZD',
     'VERIFY_TOKEN': 'verify_nbchatbot',
-    'SERVER_URL': 'https://naive-bayesian-chatbot.herokuapp.com/'
+    'SERVER_URL': 'heroku app url'
 }
